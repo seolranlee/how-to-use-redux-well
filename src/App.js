51143ad2bc78619cc441919a2536c414e2b768d6
seolranlee <leesugar90@gmail.com>
2022-02-11 18:22:29 +0900

    add constate

--- src/App.js
+++ src/App.js
@@ -1,98 +1,45 @@
+import constate from "constate"
 import { createContext, useContext, useEffect, useState } from "react"
 
-const UserContext= createContext(null)
-const UserUpdateContext = createContext(null)
-
-function UserProvider({ children }) {
-  const [user, setUser] = useState(null)
-  const [count, setCount] = useState(0)
-  return (
-    <UserContext.Provider value={{user, count}}>
-      <UserUpdateContext.Provider value={{setUser, setCount}}>
-        {children}
-      </UserUpdateContext.Provider>
-    </UserContext.Provider>
-  ) 
-}
-
+// Context를 따로 만들지 않고, 관리하고 싶은 상태를 위한 Hook 작성
 function useUser() {
-  return useContext(UserContext)
+  const [user, setUser] = useState(null)
+  return { user, setUser }
 }
 
-function useUserUpdate() {
-  return useContext(UserUpdateContext)
-}
+// useUser Hook을 기반으로 종류별로 Context를 만들고
+// 해당 Context를 사용하는 Provider와 Hook생성
+const [UserProvider, useUserValue, useUserUpdate] = constate(
+  useUser,
+  value => value.user,
+  value => value.setUser
+)
 
 function UserInfo() {
   useEffect(() => {
     console.log('UserInfo rerendering')
   })
-  const { user } = useUser()
-  if (!user) return (
-    <div>
-      <h1>user</h1>
-      <div>사용자정보가 없습니다.</div>
-    </div>
-  )
-  return (
-    <div>
-      <h1>user</h1>
-      <div>{user.username}</div>
-    </div>
-  )
-}
-
-function CountInfo() {
-  useEffect(() => {
-    console.log('CountInfo rerendering')
-  })
-  const { count } = useUser()
-
-  return (
-    <div>
-      <h1>Count</h1>
-      <div>{count}</div>
-    </div>
-  )
+  const user = useUserValue()
+  if (!user) return <div>사용자 정보가 없습니다.</div>
+  return <div>{user.username}</div>
 }
 
 function Authenticate() {
   useEffect(() => {
     console.log('Authenticate rerendering')
   })
-  const { setUser } = useUserUpdate()
-  const updateUser = () => {
+  const setUser = useUserUpdate()
+  const onClick = () => {
     setUser({ username: 'seolranlee' })
   }
-  return (
-    <div>
-      <button onClick={updateUser}>사용자 인증</button>
-    </div>
-  )
-}
-
-function CountIncrease() {
-  useEffect(() => {
-    console.log('CountIncrease rerendering')
-  })
-  const { setCount } = useUserUpdate()
-  const onIncrease = () => {
-    setCount(prev => prev + 1)
-  }
-  return (
-    <div>
-      <button onClick={onIncrease}>count + 1</button>
-    </div>
-  )
+  return <button onClick={onClick}>사용자 인증</button>
 }
 
 export default function App() {
   return (
     <UserProvider>
       <UserInfo />
-      <CountInfo />
       <Authenticate />
-      <CountIncrease />
     </UserProvider>
   )
 }
