5365b4b8bdda3527952ecd56870e1b99f527b9bc
seolranlee <leesugar90@gmail.com>
2022-02-11 18:15:05 +0900

    관심사가 분리되지 않은 context

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ const UserUpdateContext = createContext(null)
 
 function UserProvider({ children }) {
   const [user, setUser] = useState(null)
+  const [count, setCount] = useState(0)
   return (
-    <UserContext.Provider value={user}>
-      <UserUpdateContext.Provider value={setUser}>
+    <UserContext.Provider value={{user, count}}>
+      <UserUpdateContext.Provider value={{setUser, setCount}}>
         {children}
       </UserUpdateContext.Provider>
     </UserContext.Provider>
@@ -26,27 +27,72 @@ function UserInfo() {
   useEffect(() => {
     console.log('UserInfo rerendering')
   })
-  const user = useUser()
-  if (!user) return <div>사용자 정보가 없습니다.</div>
-  return <div>{user.username}</div>
+  const { user } = useUser()
+  if (!user) return (
+    <div>
+      <h1>user</h1>
+      <div>사용자정보가 없습니다.</div>
+    </div>
+  )
+  return (
+    <div>
+      <h1>user</h1>
+      <div>{user.username}</div>
+    </div>
+  )
+}
+
+function CountInfo() {
+  useEffect(() => {
+    console.log('CountInfo rerendering')
+  })
+  const { count } = useUser()
+
+  return (
+    <div>
+      <h1>Count</h1>
+      <div>{count}</div>
+    </div>
+  )
 }
 
 function Authenticate() {
   useEffect(() => {
     console.log('Authenticate rerendering')
   })
-  const setUser = useUserUpdate()
-  const onClick = () => {
+  const { setUser } = useUserUpdate()
+  const updateUser = () => {
     setUser({ username: 'seolranlee' })
   }
-  return <button onClick={onClick}>사용자 인증</button>
+  return (
+    <div>
+      <button onClick={updateUser}>사용자 인증</button>
+    </div>
+  )
+}
+
+function CountIncrease() {
+  useEffect(() => {
+    console.log('CountIncrease rerendering')
+  })
+  const { setCount } = useUserUpdate()
+  const onIncrease = () => {
+    setCount(prev => prev + 1)
+  }
+  return (
+    <div>
+      <button onClick={onIncrease}>count + 1</button>
+    </div>
+  )
 }
 
 export default function App() {
   return (
     <UserProvider>
       <UserInfo />
+      <CountInfo />
       <Authenticate />
+      <CountIncrease />
     </UserProvider>
   )
 }
